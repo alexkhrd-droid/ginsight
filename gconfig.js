7a9419bac10b74a88c41a1e624dbd19b629cdd2e
alexkhrd-droid <alexkhrd@gmail.com>
2025-09-30 14:39:02 +0300

    Create gconfig.js

---
+++ gconfig.js
@@ -0,0 +1,17 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/12.3.0/firebase-app.js';
+
+ 
+const firebaseConfig = {
+  apiKey: "AIzaSyCD11Bm8Z38U3lApRTgbSw-m-9tV4boaqM",
+  authDomain: "new-blog-ea837.firebaseapp.com",
+  projectId: "new-blog-ea837",
+  storageBucket: "new-blog-ea837.firebasestorage.app",
+  messagingSenderId: "833721509241",
+  appId: "1:833721509241:web:3a1be95a13170236c6bba8"
+};
+
+ 
+const firebaseApp = initializeApp(firebaseConfig);
+
+ 
+export { firebaseApp };
